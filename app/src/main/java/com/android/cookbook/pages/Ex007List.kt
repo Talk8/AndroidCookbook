4e8ea6fcf920d63069fc554191ff69aa9e78d856
Talk8 <error365@126.com>
2023-10-29 20:52:16 +0800

    0025 add the example of listview.

--- app/src/main/java/com/android/cookbook/pages/Ex007List.kt
+++ app/src/main/java/com/android/cookbook/pages/Ex007List.kt
@@ -1,22 +1,12 @@
 package com.android.cookbook.pages
 
-import android.widget.GridView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.magnifier
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
