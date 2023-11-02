63f466314ef08d7625758b82bc551195c71505d8
Talk8 <error365@126.com>
2023-11-02 18:55:11 +0800

    0030 add the example of switch.

--- app/src/main/java/com/android/cookbook/pages/Ex010Switch.kt
+++ app/src/main/java/com/android/cookbook/pages/Ex010Switch.kt
@@ -1,6 +1,5 @@
 package com.android.cookbook.pages
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,10 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
